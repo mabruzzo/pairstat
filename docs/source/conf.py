51b12c2419359f5df4227e8c3ce81d01b1d8155e
Matthew Abruzzo <matthewabruzzo@gmail.com>
2024-09-05 09:05:12 -0600

    automatically set the version.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,13 +3,16 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# borrowed from https://setuptools-scm.readthedocs.io/en/stable/usage/
+from importlib.metadata import version as get_version
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'pyvsf'
-copyright = '2024, Matthew Abruzzo'
-author = 'Matthew Abruzzo'
-release = '0.2'
+copyright = '2024, Matthew W. Abruzzo'
+author = 'Matthew W. Abruzzo'
+release = get_version(project)
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
