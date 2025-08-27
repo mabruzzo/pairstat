5a7f958f16261edbadc268d9984184ff95c5caba
Matthew Abruzzo <matthewabruzzo@gmail.com>
2025-08-27 13:29:42 -0400

    try to add pairstat crate as a dependency

--- impl_rs/rust/src/lib.rs
+++ impl_rs/rust/src/lib.rs
@@ -1,4 +1,19 @@
 use pyo3::prelude::*;
+use pairstat::Accumulator;
+
+#[pyclass]
+struct PyAccumulator {
+    accum: Accumulator
+}
+
+#[pymethods]
+impl PyAccumulator {
+    /// Reset the tracked accumulator state
+    fn reset_data(mut self_: PyRefMut<Self>) {
+        self_.accum.reset_data()
+    }
+}
+
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -10,5 +25,6 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 #[pymodule]
 fn _pairstat_rs_pybind(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<PyAccumulator>()?;
     Ok(())
 }
